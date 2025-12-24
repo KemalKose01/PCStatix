79d569e0189cf25c89d2ff08a2a63292cfc9d71c
Kemal Köse <xcusemellon2@gmail.com>
2025-12-24 22:12:25 +0300

    Add files via upload

---
+++ GpuStressTest.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HardwareMonitor
+{
+    /// <summary>
+    /// Interaction logic for GpuStressTest.xaml
+    /// </summary>
+    public partial class GpuStressTest : UserControl
+    {
+        public GpuStressTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
